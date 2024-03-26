0195a2b8e3cc9bea9557dea7027a6692c11ecd70
Paul Jobson <pjobson@gmail.com>
2024-03-26 03:56:56 -0400

    updated

--- index.js
+++ index.js
@@ -15,30 +15,14 @@ const googWebFontDl = async (options) => {
   options = (typeof options === "string") ? { font: options } : options;
 
   if (options.font == null) {
-    throw new Error("You need to give a font name as downloader('name') or downloader({ font: 'name' })");
+    throw new Error("You need to give a font name as downloader({ font: 'name' })");
   }
 
   options.destination = (options.destination == null) ? options.font : options.destination;
   options.destination = path.resolve(options.destination);
 
-  let formats = options.formats;
-  if (formats == null || formats === "all") {
-    formats = allFormats;
-  }
-
-  if (formats.length === 0) {
-    throw new Error("please select at least one format (or -a for all formats)");
-  }
-
-  for (const fmt of formats) {
-    if (!cssParser.userAgentMap[fmt]) {
-      throw new Error(`Unknown format “${fmt}”`);
-    }
-  }
-
-  if (!options.styles) {
-    options.styles = allStyles;
-  }
+  options.formats = allFormats;
+  options.styles = allStyles;
 
   let url = `https://fonts.googleapis.com/css?family=${querystring.escape(options.font)}:${options.styles}`
 
@@ -46,13 +30,7 @@ const googWebFontDl = async (options) => {
     url = `${url}&subset=${querystring.escape(options.subset)}`;
   }
 
-  if (options.verbose) {
-    console.log(`Downloading webfont formats: "${formats}" to folder "${options.destination}"`);
-  }
-
-  if (options.proxy) {
-    request = request.defaults({"proxy": options.proxy});
-  }
+  console.log(`Downloading webfont formats: "${options.formats}" to folder "${options.destination}"`);
 
   cssParser(options, url).then(async fontlist => {
     fontDownloader(options, fontlist.flat());
